dd5a886b78b299e0c9d653eabb8fe5da682440f0
analubarreto <analuibm@gmail.com>
2019-02-15 01:04:11 -0200

    proteção sql injection

--- banco-usuario.php
+++ banco-usuario.php
@@ -4,6 +4,9 @@ function buscaUsuario($conexao, $email, $senha) {
 	// transformar senha em md5
 	$senhaMd5 = md5($senha);
 
+	// Tratamento de SQL injection
+	$email = mysqli_real_escape_string($conexao, $email);
+
 	$query = "SELECT * FROM usuarios WHERE email='{$email}' AND senha = '{$senhaMd5}'";
 	$resultado = mysqli_query($conexao, $query);
 	$usuario = mysqli_fetch_assoc($resultado);
